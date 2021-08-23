1d7c1a15eb8693b5da07d83ea2ac2d2ed2482cf2
Andre Lustosa <alustos@ncsu.edu>
2021-08-23 12:04:17 -0400

    add sample code

---
+++ src/tests/testCalculator.py
@@ -0,0 +1,26 @@
+from simpleCalculator import add, subtract, multiply, divide
+import pytest
+
+def test_simpleCalculator_add():
+    x=5
+    y=6
+    z = add(x,y)
+    assert z == 11,"test failed"
+
+def test_simpleCalculator_subtract():
+    x=10
+    y=6
+    z = subtract(x,y)
+    assert z == 4,"test failed"
+
+def test_simpleCalculator_multiply():
+    x=5
+    y=6
+    z = multiply(x,y)
+    assert z == 30,"test failed"
+
+def test_simpleCalculator_divide():
+    x=30
+    y=6
+    z = divide(x,y)
+    assert z == 5,"test failed"
